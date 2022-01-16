bd17a5b4ee07b2c5932545e8927ae5603242c8b9
Rafa ✏️ <adanescollante@gmail.com>
2022-01-15 21:38:11 -0400

    updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="Main">
+      <Head>
+          <meta charSet="UTF-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <meta name="description" content="NextJS app"/>
+          <meta name="keywords" content="NextJS, TypeScript, Tailwind"/>
+          <meta name="author" content="Rafa"/>
+          <link rel="shortcut icon" href="../public/favicon.ico"/>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
