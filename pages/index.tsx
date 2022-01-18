8425bbd053a8de90c5102518ede457db7b8fe7f6
Rafa ✏️ <adanescollante@gmail.com>
2022-01-18 15:00:17 -0400

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,10 @@ export default function Home() {
     <div>
       <Head>
         <title>NextJS, TypeScript and Tailwind CSS template</title>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="NextJS app" />
+        <meta name="keywords" content="NextJS, TypeScript, Tailwind" />
+        <link rel="shortcut icon" href="/public/favicon.ico" />
       </Head>
       <p>Hello world</p>
     </div>
