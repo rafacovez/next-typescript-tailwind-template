8e5d315dfe5a24150803514c0768dd6071fc9568
Rafa ✏️ <adanescollante@gmail.com>
2022-01-22 21:06:16 -0400

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
 export default function Home() {
   return (
     <div>
-      <p>Hello world!</p>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid iste,
+        asperiores quisquam expedita sunt excepturi.
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quia
+        voluptatibus in totam quaerat explicabo minus ab impedit doloribus id
+        blanditiis facilis provident maxime voluptate, rem, ipsam veniam
+        obcaecati sapiente quos minima. Exercitationem, sapiente velit. Quam a
+        quo sunt provident?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quia
+        voluptatibus in totam quaerat explicabo minus ab impedit doloribus id
+        blanditiis facilis provident maxime voluptate, rem, ipsam veniam
+        obcaecati sapiente quos minima. Exercitationem, sapiente velit. Quam a
+        quo sunt provident?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quia
+        voluptatibus in totam quaerat explicabo minus ab impedit doloribus id
+        blanditiis facilis provident maxime voluptate, rem, ipsam veniam
+        obcaecati sapiente quos minima. Exercitationem, sapiente velit. Quam a
+        quo sunt provident?
+      </p>
     </div>
   )
 }
