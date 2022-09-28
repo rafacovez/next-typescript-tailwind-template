a15e7a55a220e92f320138a5324ab09d5e5175eb
Rafa ✏️ <adanescollante@gmail.com>
2022-09-28 19:20:25 -0400

    Personalized tailwind.config and global CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      mobile: '427px',
+      tablet: '743px',
+      desktop: '1512px',
+    },
   },
   plugins: [],
 }
